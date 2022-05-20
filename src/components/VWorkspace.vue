e690c470d47ce11c5808b495443db60b5655c318
Nikolai Kim <nikolai.kim@viamo.io>
2022-05-20 21:04:46 +0700

    initial commit

---
+++ src/components/VWorkspace.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="v-workspace"
+    :style="{
+      width: width + 'px',
+      height: height + 'px',
+    }"
+  >
+    <VCanvas :target-width="width" :target-height="height">
+      <slot></slot>
+    </VCanvas>
+  </div>
+</template>
+
+<script>
+import VCanvas from "./VCanvas.vue";
+
+export default {
+  components: {
+    VCanvas,
+  },
+  props: {
+    width: Number,
+    height: Number,
+  },
+};
+</script>
+
+<style scoped>
+.v-workspace {
+  position: absolute;
+  overflow: scroll;
+}
+</style>
